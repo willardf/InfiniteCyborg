6de454807be5af701c1b80a1f285d82a3c2e4f0e
Forest Willard <willardf@onid.orst.edu>
2014-12-31 17:34:03 -0800

    Remove Experience. We'll focus on component "leveling"

--- InfiniteCyborg/GameCore/Mover.cs
+++ InfiniteCyborg/GameCore/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InfCy.Maths;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +14,6 @@ namespace InfCy.GameCore
 
         public int Health = 10;
         public int MaxHealth = 10;
-        public uint Experience { get; private set; }
-        public uint ExpYield { get; set; }
 
         public int Steps { get; private set; }
         public string Name { get; set; }
@@ -26,19 +25,21 @@ namespace InfCy.GameCore
 
         public Mover()
         {
-            ExpYield = 10;
             id = MoverIds++;
             Name = "Mover " + id;
         }
 
-        public void Move(int dx, int dy, bool updateStep = false)
+        public bool Move(int dx, int dy, bool updateStep = false)
         {
             if (Game.CurrentGame.Walkable(X + dx, Y + dy))
             {
                 X += dx;
                 Y += dy;
                 if (updateStep) Steps++;
+                return true;
             }
+
+            return false;
         }
 
         public void SetPosition(int x, int y)
@@ -51,17 +52,10 @@ namespace InfCy.GameCore
 
         protected virtual void OnDeath(Mover killer)
         {
-            Logger.Log("{0} killed {1}. Gained {2}xp.", killer.Name, this.Name, this.ExpYield);
-            killer.GainExperience(ExpYield);
+            Logger.Log("{0} killed {1}.", killer.Name, this.Name);
         }
 
-        private void GainExperience(uint xp)
-        {
-            Experience += xp;
-            // TODO: Levels
-        }
-
-        internal void TakeDamage(int dmg, Mover attacker)
+        public void TakeDamage(int dmg, Mover attacker)
         {
             this.Health -= dmg;
             if (this.Health <= 0)
@@ -69,5 +63,30 @@ namespace InfCy.GameCore
                 OnDeath(attacker);
             }
         }
+
+        public void Push(IntVector dir, int amount, Mover cause)
+        {
+            int dmg = 0;
+            for (int i = 0; i < amount;  ++i)
+            {
+                if (!Move(dir.X, dir.Y))
+                {
+                    dmg = amount - i;
+                    break;
+                }
+            }
+
+            if (dmg > 0)
+            {
+                Logger.Log("{0} was knocked back and took {1} damage!", this.Name, dmg);
+                this.TakeDamage(dmg, cause);
+            }
+            else
+            {
+                Logger.Log("{0} was knocked back!", this.Name);
+            }
+        }
+
+        public IntVector Position { get { return new IntVector(X, Y); } }
     }
 }
